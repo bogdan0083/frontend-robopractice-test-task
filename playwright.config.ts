a3ca2934162e11e884a8eac0dd9d3c86607221be
Bogdan Dolin <d.bogdan@inbox.ru>
2022-09-12 11:17:10 +0700

    Move server port back to 8080

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,27 +1,26 @@
 import { PlaywrightTestConfig } from '@playwright/test'
 
-
-
-
-
 const config: PlaywrightTestConfig = {
-  testDir: "./__tests__/e2e",
+  testDir: './__tests__/e2e',
   retries: 3,
-  webServer: [{
-    command: "npx vite --port 5174",
-    port: 5174,
-    reuseExistingServer: !process.env.CI,
-  }, {
-    command: "PORT=8081 npm run server",
-    port: 8081,
-    reuseExistingServer: !process.env.CI,
-  }],
+  webServer: [
+    {
+      command: 'npx vite --port 5174',
+      port: 5174,
+      reuseExistingServer: !process.env.CI,
+    },
+    {
+      command: 'npm run server',
+      port: 8080,
+      reuseExistingServer: !process.env.CI,
+    },
+  ],
   use: {
     headless: true,
-    baseURL: "http://localhost:5174",
-    viewport: {width: 1280, height: 720},
+    baseURL: 'http://localhost:5174',
+    viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true,
-    video: "on-first-retry",
+    video: 'on-first-retry',
   },
-};
-export default config;
+}
+export default config
